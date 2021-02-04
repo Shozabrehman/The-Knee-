59a93518fb841ed5f41b2780a58b554f9a0f25b8
Shozabrehman <78487649+Shozabrehman@users.noreply.github.com>
2021-02-04 19:01:01 +0100

    The Knee

---
+++ The Knee/Assets/ActivatingObjects.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+public class ActivatingObjects : MonoBehaviour {
+
+    public GameObject femur;
+    public GameObject patella;
+    public GameObject atricartilage;
+    public GameObject patellacart;
+    public GameObject meniscus;
+    public GameObject tinficar;
+    public GameObject tinfi;
+    public GameObject acl;
+    public GameObject pcl;
+   
+
+    bool activatefemur, activatepatella, activateatricartilage, activatepatellacart, activatemeniscus, activatetinfi, activatetinficar, activateacl, activatepcl = true;
+
+
+    public void ActivateFemur()
+    {   
+        activatefemur = !activatefemur;
+        femur.SetActive(activatefemur);
+    }
+    public void ActivePatella()
+    {
+        activatepatella = !activatepatella;
+        patella.SetActive(activatepatella);
+    }
+    public void Activeatricartilage()
+    {
+        activateatricartilage = !activateatricartilage;
+        atricartilage.SetActive(activateatricartilage);
+    }
+    public void Activatepatellacart()
+    {
+        activatepatellacart = !activatepatellacart;
+        patellacart.SetActive(activatepatellacart);
+    }
+    public void Activatemeniscus()
+    {
+        activatemeniscus = !activatemeniscus;
+        meniscus.SetActive(activatemeniscus);
+    }
+    public void Activatetinfi()
+    {
+        activatetinfi = !activatetinfi;
+        tinfi.SetActive(activatetinfi);
+    }
+    public void Activatetinficar()
+    {
+        activatetinficar = !activatetinficar;
+        tinficar.SetActive(activatetinficar);
+    }
+    public void Activateacl()
+    {
+        activateacl = !activateacl;
+        acl.SetActive(activateacl);
+    }
+    public void Áctivatepcl()
+    {
+        activatepcl = !activatepcl;
+        pcl.SetActive(activatepcl);
+    }
+
+}
---
+++ The Knee/Assets/MenuToggle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using VRTK;
+public class MenuToggle : MonoBehaviour {
+    public VRTK_ControllerEvents ControllerEvents;
+    public GameObject menu;
+
+    bool menuState = false;
+
+    void OnEnable()
+    {     
+        ControllerEvents.ButtonTwoReleased += ControllerEvents_ButtonTwoReleased;
+    }
+
+    void OnDisable()
+    {
+        ControllerEvents.ButtonTwoReleased -= ControllerEvents_ButtonTwoReleased;
+    }
+    public void ControllerEvents_ButtonTwoReleased(object sender, ControllerInteractionEventArgs e)
+    {
+        menuState = !menuState;
+        menu.SetActive(menuState);
+    }
+}
